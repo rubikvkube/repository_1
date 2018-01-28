d840be330abb678af3ce9ccc85d3a5633c937a49
Alesya <alesia.zeziulina@gmail.com>
2018-01-28 21:33:53 +0300

    new commit

---
+++ task_1.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+import os
+
+path = str(input("Enter path to directory: "))
+file_extension = str(input("Enter file extension (e.g. .txt): "))
+list_of_files = []
+for file in os.listdir(path):
+    if file.endswith(file_extension):
+        list_of_files.append(os.path.join(path, file))
+
+latest_file = max(list_of_files, key=os.path.getctime)
+
+for file in list_of_files:
+	if os.path.getmtime(latest_file)-os.path.getmtime(file)<=10:
+		print (file)
+
